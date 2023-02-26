5aec9ff749781b67ec6b8a233d5f792b29f36705
Omer Gencoglu <omersaitgencoglu@gmail.com>
2023-02-26 11:11:12 -0500

    Update DarkModeSwitch comp

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -3,5 +3,9 @@
 import { ThemeProvider } from "next-themes";
 
 export default function Providers({ children }) {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider enableSystem={true} attribute="class">
+      {children}
+    </ThemeProvider>
+  );
 }
--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -1,3 +1,19 @@
+"use client";
+
+import { Fragment } from "react";
+
+import { MdLightMode } from "react-icons/md";
+import { BsFillMoonFill } from "react-icons/bs";
+import { useTheme } from "next-themes";
+
 export default function DarkModeSwitch() {
-  return <div>DarkModeSwitch</div>;
+  const { systemTheme, theme, setTheme } = useTheme();
+
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
+  return (
+    <Fragment>
+      {currentTheme === "dark" ? <MdLightMode /> : <BsFillMoonFill />}
+    </Fragment>
+  );
 }
